e19f311e476e8ebbccda2ecc869425ec50e6c5fd
Prueba <andr.marin@duocuc.cl>
2026-07-08 16:19:44 -0400

    hateoas en envio

--- MicroService-Envio/src/main/java/cl/EvaluacionParcial3/MicroService_Envio/Controller/EnvioController.java
+++ MicroService-Envio/src/main/java/cl/EvaluacionParcial3/MicroService_Envio/Controller/EnvioController.java
@@ -11,11 +11,16 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping("/v1/envios")
@@ -25,6 +30,7 @@ public class EnvioController {
     @Autowired
     private EnvioService envioService;
 
+
     @GetMapping("/usuario/{usuarioId}")
     @Operation(summary = "Obtener envio por usuario por ID", description = "Obtiene un envio mediante el ID del usuario")
     @ApiResponses(value = {
@@ -32,10 +38,28 @@ public class EnvioController {
             @ApiResponse(responseCode = "404", description = "Envio no encontrada"),
             @ApiResponse(responseCode = "500", description = "Error interno del servidor")
     })
-    public List<EnvioResponse> obtenerEnviosPorUsuarioId(@PathVariable Long usuarioId) {
+    public CollectionModel<EntityModel<EnvioResponse>> obtenerEnviosPorUsuarioId(
+            @PathVariable Long usuarioId) {
+
         log.info("GET /v1/envios/usuario/{}", usuarioId);
-        return envioService.obtenerEnvioPorUsuarioId(usuarioId);
+
+        List<EntityModel<EnvioResponse>> envios = envioService.obtenerEnvioPorUsuarioId(usuarioId)
+                .stream()
+                .map(envio -> EntityModel.of(
+                        envio,
+                        linkTo(methodOn(EnvioController.class)
+                                .obtenerEnvioPorNumeroSeguimiento(envio.getNumeroSeguimiento()))
+                                .withSelfRel()
+                ))
+                .toList();
+
+        return CollectionModel.of(envios,
+                linkTo(methodOn(EnvioController.class)
+                        .obtenerEnviosPorUsuarioId(usuarioId))
+                        .withSelfRel()
+        );
     }
+
 
     @GetMapping
     @Operation(summary = "Obtener envios", description = "Obtiene una lista de todos los envios registrados.")
@@ -43,11 +67,32 @@ public class EnvioController {
             @ApiResponse(responseCode = "200", description = "Operacion realizada con exito"),
             @ApiResponse(responseCode = "500", description = "Error interno del servidor")
     })
-    public ResponseEntity<List<EnvioResponse>> obtenerEnvios() {
+    public ResponseEntity<CollectionModel<EntityModel<EnvioResponse>>> obtenerEnvios() {
+
         log.info("GET /v1/envios");
-        List<EnvioResponse> envios = envioService.listarTodos();
-        return ResponseEntity.ok(envios);
+
+        List<EntityModel<EnvioResponse>> envios = envioService.listarTodos()
+                .stream()
+                .map(envio -> EntityModel.of(
+                        envio,
+                        linkTo(methodOn(EnvioController.class)
+                                .obtenerEnvioPorNumeroSeguimiento(envio.getNumeroSeguimiento()))
+                                .withSelfRel()
+                ))
+                .toList();
+
+        CollectionModel<EntityModel<EnvioResponse>> response =
+                CollectionModel.of(envios);
+
+        response.add(
+                linkTo(methodOn(EnvioController.class)
+                        .obtenerEnvios())
+                        .withSelfRel()
+        );
+
+        return ResponseEntity.ok(response);
     }
+
 
     @GetMapping("/seguimiento/{numeroSeguimiento}")
     @Operation(summary = "Obtener envio por numero de seguimiento", description = "Obtiene un envio mediante el numero de seguimiento")
@@ -56,11 +101,28 @@ public class EnvioController {
             @ApiResponse(responseCode = "404", description = "Envio no encontrada"),
             @ApiResponse(responseCode = "500", description = "Error interno del servidor")
     })
-    public ResponseEntity<EnvioResponse> obtenerEnvioPorNumeroSeguimiento(@PathVariable String numeroSeguimiento) {
+    public ResponseEntity<EntityModel<EnvioResponse>> obtenerEnvioPorNumeroSeguimiento(
+            @PathVariable String numeroSeguimiento) {
+
         log.info("GET /v1/envios/seguimiento/{}", numeroSeguimiento);
-        EnvioResponse envios = envioService.obtenerEnvioPorNumeroSeguimiento(numeroSeguimiento);
-        return ResponseEntity.ok(envios);
+
+        EnvioResponse envio =
+                envioService.obtenerEnvioPorNumeroSeguimiento(numeroSeguimiento);
+
+        EntityModel<EnvioResponse> response = EntityModel.of(
+                envio,
+                linkTo(methodOn(EnvioController.class)
+                        .obtenerEnvioPorNumeroSeguimiento(numeroSeguimiento))
+                        .withSelfRel(),
+
+                linkTo(methodOn(EnvioController.class)
+                        .obtenerEnvios())
+                        .withRel("listar-envios")
+        );
+
+        return ResponseEntity.ok(response);
     }
+
 
     @PostMapping
     @Operation(summary = "Crear envio", description = "Crea un nuevo envio")
@@ -72,11 +134,28 @@ public class EnvioController {
             @ApiResponse(responseCode = "409", description = "El envio ya existe"),
             @ApiResponse(responseCode = "500", description = "Error interno del servidor")
     })
-    public ResponseEntity<EnvioResponse> crearEnvio(@Valid @RequestBody EnvioRequest envioRequest) {
+    public ResponseEntity<EntityModel<EnvioResponse>> crearEnvio(
+            @Valid @RequestBody EnvioRequest envioRequest) {
+
         log.info("POST /v1/envios");
-        EnvioResponse envioResponse = envioService.crearEnvio(envioRequest);
-        return new ResponseEntity<>(envioResponse, HttpStatus.CREATED);
+
+        EnvioResponse envio =
+                envioService.crearEnvio(envioRequest);
+
+        EntityModel<EnvioResponse> response = EntityModel.of(
+                envio,
+                linkTo(methodOn(EnvioController.class)
+                        .obtenerEnvioPorNumeroSeguimiento(envio.getNumeroSeguimiento()))
+                        .withSelfRel(),
+
+                linkTo(methodOn(EnvioController.class)
+                        .obtenerEnvios())
+                        .withRel("listar-envios")
+        );
+
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
+
 
     @PutMapping("/{id}")
     @Operation(summary = "Actualizar envio", description = "Actualiza un envio mediante su ID.")
@@ -86,13 +165,30 @@ public class EnvioController {
             @ApiResponse(responseCode = "404", description = "Envio no encontrado"),
             @ApiResponse(responseCode = "500", description = "Error interno del servidor")
     })
-    public ResponseEntity<EnvioResponse> actualizarEnvio(
+    public ResponseEntity<EntityModel<EnvioResponse>> actualizarEnvio(
             @PathVariable Long id,
             @Valid @RequestBody EnvioRequest envioRequest) {
+
         log.info("PUT /v1/envios/{}", id);
-        EnvioResponse envioResponse = envioService.actualizarEnvio(id, envioRequest);
-        return ResponseEntity.ok(envioResponse);
+
+        EnvioResponse envio =
+                envioService.actualizarEnvio(id, envioRequest);
+
+
+        EntityModel<EnvioResponse> response = EntityModel.of(
+                envio,
+                linkTo(methodOn(EnvioController.class)
+                        .obtenerEnvioPorNumeroSeguimiento(envio.getNumeroSeguimiento()))
+                        .withSelfRel(),
+
+                linkTo(methodOn(EnvioController.class)
+                        .obtenerEnvios())
+                        .withRel("listar-envios")
+        );
+
+        return ResponseEntity.ok(response);
     }
+
 
     @DeleteMapping("/{id}")
     @Operation(summary = "Eliminar envio", description = "Elimina un envio mediante su ID")
@@ -102,8 +198,11 @@ public class EnvioController {
             @ApiResponse(responseCode = "500", description = "Error interno del servidor")
     })
     public ResponseEntity<Void> eliminarEnvio(@PathVariable Long id) {
+
         log.info("DELETE /v1/envios/{}", id);
+
         envioService.eliminarEnvio(id);
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+
+        return ResponseEntity.noContent().build();
     }
 }
